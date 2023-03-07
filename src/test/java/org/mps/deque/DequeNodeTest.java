bb1c2585b8bef9655379eb66fe526684026b83ab
DavidHQ-sudo <davidvilaseca10@gmail.com>
2023-03-07 19:13:14 +0100

    Anyadir test para DequeNodeTests

--- src/test/java/org/mps/deque/DequeNodeTest.java
+++ src/test/java/org/mps/deque/DequeNodeTest.java
@@ -12,13 +12,13 @@ class DequeNodeTest {
     Integer item;
 
     @Test
-    @DisplayName("is instatiated with new DoublyLinkedListDeque<>()")
+    @DisplayName("is instatiated with new DequeNodeTest<>()")
     void isInstatiatedWithNew() {
         new DequeNode<Integer>(item,prev,next);
     }
 
     @Nested
-    @DisplayName("when new")
+    @DisplayName("when prev and next are terminal")
     class whenNew {
         @BeforeEach
         void setUp() {
@@ -37,6 +37,19 @@ class DequeNodeTest {
         void isLastEmpty() {
             assertTrue(list.isFirstNode());
         }
+    }
+
+    @Test
+    @DisplayName("when prev and next are no terminal")
+    void isNotATerminalNode() {
+        item = 5;
+        DequeNode<Integer> prev2 = new DequeNode<>(item,null,prev);
+        prev = new DequeNode<>(item,prev2,list);
+        DequeNode<Integer> next2 = new DequeNode<>(item,next,null);
+        next = new DequeNode<>(item,list,next2);
+        list = new DequeNode<>(item,prev,next);
+        assertTrue(list.isNotATerminalNode());
+
     }
 
 }
