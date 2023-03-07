549506b9ee453afe04536c3ae531c02e3bf46b89
LAPTOP-PH3L3CSL\cgmoy <carlosgmoya@uma.es>
2023-03-07 11:42:34 +0100

    DoublyLinkedListDeque

--- src/main/java/org/mps/deque/DoublyLinkedListDeque.java
+++ src/main/java/org/mps/deque/DoublyLinkedListDeque.java
@@ -7,44 +7,90 @@ public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
     private int size;
 
     public DoublyLinkedListDeque() {
-        // TODO
+        this.first = null;
+        this.last = null;
+        size = 0;
     }
 
     @Override
     public void prepend(T value) {
-        // TODO
+        DequeNode<T> node;
+        if(size == 0) {
+            node = new DequeNode<T>(value, null, null);
+            this.first = node;
+            this.last = node;
+        } else {
+            node = new DequeNode<T>(value, null, first);
+            first.setPrevious(node);
+            this.first = node;
+        }
+        size++;
     }
 
     @Override
     public void append(T value) {
-        // TODO
+        DequeNode<T> node;
+        if(size == 0) {
+            node = new DequeNode<T>(value, null, null);
+            this.first = node;
+            this.last = node;
+        } else {
+            node = new DequeNode<T>(value, last, null);
+            last.setNext(node);
+            this.last = node;
+        }
+        size++;
+
     }
 
     @Override
     public void deleteFirst() {
-        // TODO
+        if (size == 0){
+            throw new DoubleEndedQueueException("La lista esta vacia");
+        } else if(size == 1) {
+            this.first = null;
+            size--;
+        } else {
+            first.getNext().setPrevious(null);
+            this.first = first.getNext();
+            size--;
+        }
     }
 
     @Override
     public void deleteLast() {
-        // TODO
+        if (size == 0) {
+            throw new DoubleEndedQueueException("La lista esta vacia");
+        } else if(size == 1) {
+            this.last = null;
+            size--;
+        } else {
+            last.getPrevious().setNext(null);
+            this.last = last.getPrevious();
+            size--;
+        }
     }
 
     @Override
     public T first() {
-        // TODO
-        return null;
+        if(size == 0){
+            throw new DoubleEndedQueueException("La lista esta vacia");
+        } else {
+            return this.first.getItem();
+        }
     }
 
     @Override
     public T last() {
-        // TODO
-        return null;
+        if(size == 0){
+            throw new DoubleEndedQueueException("La lista esta vacia");
+        } else {
+            return this.last.getItem();
+        }
     }
 
     @Override
     public int size() {
-        // TODO
-        return 0;
+        return this.size;
     }
 }
