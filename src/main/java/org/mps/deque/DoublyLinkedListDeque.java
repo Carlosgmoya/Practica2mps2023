f57e1a3685784ccbf90e78085341ea8e7e9f5aea
LAPTOP-PH3L3CSL\cgmoy <carlosgmoya@uma.es>
2023-03-14 12:28:36 +0100

    remove() and sort() implementation

--- src/main/java/org/mps/deque/DoublyLinkedListDeque.java
+++ src/main/java/org/mps/deque/DoublyLinkedListDeque.java
@@ -128,11 +128,73 @@ public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
 
     @Override
     public void remove(T value) {
-
+        if(size == 0){
+            throw new DoubleEndedQueueException("List is empty");
+        }
+        boolean found = false;
+        int i = 0;
+        DequeNode<T> aux = first;
+        while (!found && i < size) {
+            if (this.get(i).equals(value)) {
+                found = true;
+                if(i == 0) {
+                    aux.getNext().setPrevious(null);
+                    this.first = aux.getNext();
+                    size--;
+                } else if(i == size - 1) {
+                    aux.getPrevious().setNext(null);
+                    this.last = aux.getPrevious();
+                    size--;
+                } else {
+                    aux.getPrevious().setNext(aux.getNext());
+                    aux.getNext().setPrevious(aux.getPrevious());
+                    size--;
+                }
+            } else {
+                aux = aux.getNext();
+                i++;
+            }
+        }
+        if(!found){
+            throw new DoubleEndedQueueException("The list doesn't contain value " + value);
+        }
     }
 
     @Override
     public void sort(Comparator<? super T> comparator) {
-
+        if(size == 0){
+            throw new DoubleEndedQueueException("List is empty");
+        } else if(size == 1){
+            throw new DoubleEndedQueueException("List only has 1 item");
+        }
+        DequeNode<T> current = first.getNext();
+        DequeNode<T> aux;
+        int i = 1;
+        while (i < size){
+            if(comparator.compare(current.getItem(), current.getPrevious().getItem()) < 0) {
+                if(current.getPrevious().isFirstNode()) {
+                    aux = current.getPrevious();
+                    aux.setNext(current.getNext());
+                    current.setNext(aux);
+                    current.setPrevious(aux.getPrevious());
+                    aux.setPrevious(current);
+                    first = current;
+                    current = current.getNext();
+                } else {
+                    aux = current.getPrevious();
+                    aux.setNext(current.getNext());
+                    current.setNext(aux);
+                    current.setPrevious(aux.getPrevious());
+                    aux.setPrevious(current);
+                    current = aux;
+                }
+            } else {
+                i++;
+                if(current.isLastNode() && (comparator.compare(current.getItem(), last.getItem()) != 0)) {
+                    current = last;
+                }
+                current = current.getNext();
+            }
+        }
     }
 }
